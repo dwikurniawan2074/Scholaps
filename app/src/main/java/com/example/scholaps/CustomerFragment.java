80436ccce41fa4178ca14f71048acd39b0811a1a
dwikurniawan2074 <dwikurniawan.2074@students.unila.ac.id>
2022-06-26 22:00:34 +0700

    Update Input Data Pendaftaran Beasiswa dan Hubungi Customer Service By Email

--- app/src/main/java/com/example/scholaps/CustomerFragment.java
+++ app/src/main/java/com/example/scholaps/CustomerFragment.java
@@ -1,64 +1,56 @@
 package com.example.scholaps;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CustomerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CustomerFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CustomerFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CustomerFragment newInstance(String param1, String param2) {
-        CustomerFragment fragment = new CustomerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    Button btnEmail, btnLivechat, btnFaq;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false);
+        View view = inflater.inflate(R.layout.fragment_customer, container, false);
+        btnEmail = view.findViewById(R.id.btn_email);
+        btnLivechat = view.findViewById(R.id.btn_livechat);
+        btnFaq = view.findViewById(R.id.btn_faq);
+        Fragment selectedFragment = null;
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"scholapsdeveloper@gmail.com"});
+                intent.putExtra(Intent.EXTRA_CC, new String[] {"scholapsdeveloper2@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Scholaps Contact");
+                intent.putExtra(Intent.EXTRA_TEXT, "Halo Admin Scholaps! Saya ingin menyampaikan sebuah pernyataan...(isi sesuai yang kalian inginkan!)");
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    //do something else
+                }
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/example/scholaps/PendaftaranFragment.java
+++ app/src/main/java/com/example/scholaps/PendaftaranFragment.java
@@ -1,64 +1,111 @@
 package com.example.scholaps;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PendaftaranFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class PendaftaranFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String[] daftar;
+    protected Cursor cursor;
+    public static PendaftaranFragment pf;
+    Database database;
+    Button btn_daftar, btn_lihat;
+    EditText nama_beasiswa, nama, alamat, email, asal_sekolah;
+    ListView listView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PendaftaranFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PendaftaranFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PendaftaranFragment newInstance(String param1, String param2) {
-        PendaftaranFragment fragment = new PendaftaranFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    String[] items = {"Djarum Foundation","Lazada Forward","AMINEF","Sariraya Japan 2022","Beasiswa JIS", "Beasiswa Brunei Darussalam", "Mitsui Bussan"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pendaftaran, container, false);
+        View view = inflater.inflate(R.layout.fragment_pendaftaran, container, false);
+        database = new Database(getActivity());
+        nama_beasiswa = view.findViewById(R.id.nama_beasiswa);
+        nama = view.findViewById(R.id.nama);
+        alamat = view.findViewById(R.id.alamat);
+        email = view.findViewById(R.id.email);
+        asal_sekolah = view.findViewById(R.id.asal_sekolah);
+        btn_daftar = view.findViewById(R.id.btn_daftar);
+        pf = this;
+
+        Fragment selectedFragment = null;
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        autoCompleteTextView = view.findViewById(R.id.nama_beasiswa);
+
+        adapterItems = new ArrayAdapter<String>(getActivity(), R.layout.list_item, items);
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,  View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+        });
+
+
+        btn_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = database.getWritableDatabase();
+                db.execSQL("insert into pendaftar(nama_beasiswa, nama, alamat, email, asal_sekolah) values('" +
+                        nama_beasiswa.getText().toString() + "','" +
+                        nama.getText().toString() + "','" +
+                        alamat.getText().toString() + "','" +
+                        email.getText().toString() + "','" +
+                        asal_sekolah.getText().toString() + "')");
+                Toast.makeText(getActivity(), "Data Tersimpan!", Toast.LENGTH_SHORT).show();
+                PendaftaranFragment.pf.RefreshList();
+            }
+        });
+
+
+
+        RefreshList();
+
+
+
+
+
+        return view;
+    }
+
+    public void RefreshList() {
+        SQLiteDatabase db = database.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM pendaftar", null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for(int i=0; i<cursor.getCount(); i++){
+            cursor.moveToPosition(i);
+            daftar[i] = cursor.getString(0 ).toString();
+        }
     }
 }
